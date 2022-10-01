8468499cd18e4da9ff825a7a9a00c1329059bc68
arqkm <maheshwary.arqk@gmail.com>
2022-10-01 16:45:38 +0400

    feat: Displaying current time on server

--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("<h1> Hi This is my first Project in Django. </h1>")
 
-
+def timer(request):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return HttpResponse("Current Time = "+ current_time)
